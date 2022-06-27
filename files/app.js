95cfb66caccc2daea23d8db5f1ebafc6d841a70e
alphanse <alphanse98@gmail.com>
2022-06-27 16:10:38 +0530

    add-file

---
+++ files/app.js
@@ -0,0 +1,19 @@
+let a = parseInt(prompt())
+if(a >= 90){
+    console.log("your grade is A ");
+}else if(a >= 80){
+    console.log("your grade is b ");
+}else if(a >= 70){
+    console.log("your grade is c ");
+}else if(a >= 60){
+    console.log("your grade is d ");
+}else if(a >= 50){
+    console.log("your grade is e ");
+}
+else{
+    console.log("your grade is u ");
+}
+
+
+
+
